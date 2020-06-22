7244b9c4a6356afba615fb0aca100dca2a4097b4
Viona <vionamahdiya@gmail.com>
2020-06-22 16:25:40 +0700

    Fixing error

--- app/src/main/java/com/vionavio/mytestingapp/MainActivity.kt
+++ app/src/main/java/com/vionavio/mytestingapp/MainActivity.kt
@@ -2,10 +2,38 @@ package com.vionavio.mytestingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import java.lang.StringBuilder
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnSetValue: Button
+    private lateinit var tvText: TextView
+
+    private var names = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvText = findViewById(R.id.tv_text)
+        btnSetValue = findViewById(R.id.btn_set_value)
+        btnSetValue.setOnClickListener(this)
+
+        names.add("Viona Mahdiya")
+        names.add("Kevin")
+        names.add("Yoza")
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_set_value){
+            //tvText.text = "19"
+            val name = StringBuilder()
+            for (i in 0..2){
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
+        }
     }
 }
