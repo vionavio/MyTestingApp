bee80d20fe67f71d275f2141e04808c4fb1a36d4
Viona <vionamahdiya@gmail.com>
2020-06-22 21:18:51 +0700

    Fixing error outOfMemory

--- app/src/main/java/com/vionavio/mytestingapp/MainActivity.kt
+++ app/src/main/java/com/vionavio/mytestingapp/MainActivity.kt
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnSetValue: Button
     private lateinit var tvText: TextView
+    private lateinit var imgPreview: ImageView
 
     private var names = ArrayList<String>()
 
@@ -24,13 +28,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         names.add("Viona Mahdiya")
         names.add("Kevin")
         names.add("Yoza")
+
+        imgPreview = findViewById(R.id.img_preview)
+        //imgPreview.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.fronalpstock_big))
+        Glide.with(this).load(R.drawable.fronalpstock_big).into(imgPreview)
     }
 
     override fun onClick(v: View) {
-        if (v.id == R.id.btn_set_value){
+        if (v.id == R.id.btn_set_value) {
             //tvText.text = "19"
             val name = StringBuilder()
-            for (i in 0..2){
+            for (i in 0..2) {
                 name.append(names[i]).append("\n")
             }
             tvText.text = name.toString()
